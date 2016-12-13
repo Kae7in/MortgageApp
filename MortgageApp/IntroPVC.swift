c0a36abd9d79d6c36a702e6a156ee301f5ab5381
Kaelin Hooper <kaelin.hooper@roberts-MacBook-Pro.local>
2016-12-13 12:14:12 -0600

    Generalized the construction of multiple view controllers for the intro/tutorial screen

--- MortgageApp/IntroPVC.swift
+++ MortgageApp/IntroPVC.swift
@@ -13,8 +13,6 @@ class IntroPVC: UIPageViewController {
     private(set) lazy var orderedViewControllers: [UIViewController] = {
         return [self.newIntroVC(num: 1), self.newIntroVC(num: 2), self.newIntroVC(num: 3)]
     }()
-    var horizontalLine: UIView? = nil
-    var label: UILabel? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,55 +24,14 @@ class IntroPVC: UIPageViewController {
         if let firstViewController = orderedViewControllers.first {
             setViewControllers([firstViewController], direction: .forward, animated: true, completion: nil)
         }
-//        setBackgroundColors()
-//        drawHorizontalLine()
-//        addMessage()
     }
     
     private func newIntroVC(num: Int) -> UIViewController {
-        return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "IntroVC\(num)")
-    }
-    
-    func setBackgroundColors() {
-        let gradient = CAGradientLayer()
-        gradient.frame = view.bounds
-        gradient.colors = [UIColor.init(rgbColorCodeRed: 39, green: 227, blue: 233, alpha: 1).cgColor, UIColor.init(rgbColorCodeRed: 0, green: 148, blue: 180, alpha: 1).cgColor]
-        gradient.startPoint = CGPoint(x: 0.5, y: 0.0)
-        gradient.endPoint = CGPoint(x: 0.5, y: 1.0)
-        view.layer.insertSublayer(gradient, at: 0)
-        //use startPoint and endPoint to change direction of gradient (http://stackoverflow.com/a/20387923/2057171)
+        var vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "IntroVC\(num)") as! IntroDetailVC
         
-        UIApplication.shared.statusBarStyle = .lightContent
-    }
-    
-    func drawHorizontalLine() {
-        let lineFrame = CGRect(x: 0.0, y: (view.frame.height / 2.0) - 35.0, width: view.frame.width, height: 1.0)
-        horizontalLine = UIView(frame: lineFrame)
-        horizontalLine?.backgroundColor = UIColor.white
-        view.addSubview(horizontalLine!)
-    }
-    
-    func addMessage() {
-        let string = "Your loan is made up of principal and interest, which combine to form the remaining balance."
-        let defaultFontAttributes = [NSFontAttributeName: UIFont(name: ".SFUIDisplay-Ultralight", size: 38.0)!, NSForegroundColorAttributeName: UIColor.white]
-        let attributedString = NSMutableAttributedString(string: string, attributes: defaultFontAttributes)
-        let boldFontAttribute = [NSFontAttributeName: UIFont(name: ".SFUIDisplay-Medium", size: 38.0)!]
-
-        attributedString.addAttributes(boldFontAttribute, range: (string as NSString).range(of: "principal"))
-        attributedString.addAttributes(boldFontAttribute, range: (string as NSString).range(of: "interest"))
-        attributedString.addAttributes(boldFontAttribute, range: (string as NSString).range(of: "balance"))
+        vc.addMessage(message: num)
         
-        label = UILabel(frame: CGRect(x: 10.0, y: 0.0, width: view.frame.width - 20.0, height: CGFloat.greatestFiniteMagnitude))  // maximum height used because we will resize dynamically
-        label?.textAlignment = .left
-        label?.attributedText = attributedString
-        label?.numberOfLines = 4
-        label?.lineBreakMode = NSLineBreakMode.byTruncatingTail
-        label!.adjustsFontSizeToFitWidth = true
-        label!.minimumScaleFactor = 24.0 / label!.font.pointSize  // Dynamically resize font to fit label
-        label?.sizeToFit()  // Must dynamically size resize label to fit the text more exactly
-        let y: CGFloat = self.horizontalLine!.frame.minY - label!.frame.height
-        label!.frame = CGRect(x: label!.frame.minX, y: y - 5.0, width: label!.frame.width, height: label!.frame.height)
-        view.addSubview(label!)
+        return vc
     }
     
     override func viewWillDisappear(_ animated: Bool) {
