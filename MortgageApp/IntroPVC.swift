0c7d37abaf2155265e86da77d125ea77459a6626
Kaelin Hooper <kaelin.hooper@roberts-MacBook-Pro.local>
2016-12-13 11:04:51 -0600

    Switching to page view controller

--- MortgageApp/IntroPVC.swift
+++ MortgageApp/IntroPVC.swift
@@ -1,5 +1,5 @@
 //
-//  IntroVC.swift
+//  IntroPVC.swift
 //  MortgageApp
 //
 //  Created by Kaelin Hooper on 12/12/16.
@@ -8,18 +8,31 @@
 
 import UIKit
 
-class IntroVC: UIViewController {
+class IntroPVC: UIPageViewController {
     
+    private(set) lazy var orderedViewControllers: [UIViewController] = {
+        return [self.newIntroVC(num: 1), self.newIntroVC(num: 2), self.newIntroVC(num: 3)]
+    }()
     var horizontalLine: UIView? = nil
     var label: UILabel? = nil
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
-        setBackgroundColors()
-        drawHorizontalLine()
-        addMessage()
+        dataSource = self
+        self.automaticallyAdjustsScrollViewInsets = false
+        
+        if let firstViewController = orderedViewControllers.first {
+            setViewControllers([firstViewController], direction: .forward, animated: true, completion: nil)
+        }
+//        setBackgroundColors()
+//        drawHorizontalLine()
+//        addMessage()
+    }
+    
+    private func newIntroVC(num: Int) -> UIViewController {
+        return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "IntroVC\(num)")
     }
     
     func setBackgroundColors() {
@@ -87,4 +100,57 @@ class IntroVC: UIViewController {
     }
     */
 
+}
+
+
+extension IntroPVC: UIPageViewControllerDataSource {
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        guard previousIndex >= 0 else {
+            return nil
+        }
+        
+        guard orderedViewControllers.count > previousIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[previousIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        let orderedViewControllersCount = orderedViewControllers.count
+        
+        guard orderedViewControllersCount != nextIndex else {
+            return nil
+        }
+        
+        guard orderedViewControllersCount > nextIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[nextIndex]
+    }
+    
+    // PAGE DOTS
+//    func presentationCount(for pageViewController: UIPageViewController) -> Int {
+//        return orderedViewControllers.count
+//    }
+//    
+//    func presentationIndex(for pageViewController: UIPageViewController) -> Int {
+//        guard let firstViewController = viewControllers?.first, let firstViewControllerIndex = orderedViewControllers.index(of: firstViewController) else {
+//            return 0
+//        }
+//        
+//        return firstViewControllerIndex
+//    }
 }
