d0d4711d6f66f8d1138d7e6b5cc0300daf09605c
Kaelin Hooper <kaelin.hooper@roberts-MacBook-Pro.local>
2016-12-28 12:02:54 -0600

    ..

--- MortgageApp/EditPaymentVC.swift
+++ MortgageApp/EditPaymentVC.swift
@@ -11,10 +11,19 @@ import UIKit
 class EditPaymentVC: UIViewController {
     
     var m: Mortgage? = nil
+    var newM: Mortgage? = nil
     var mc: MortgageCalculator = MortgageCalculator()
 
     @IBOutlet weak var navbar: UINavigationItem!
+    
+    @IBOutlet weak var principalBalance: UILabel!
+    @IBOutlet weak var interestBalance: UILabel!
+    @IBOutlet weak var yearsBalance: UILabel!
+    @IBOutlet weak var monthsBalance: UILabel!
     @IBOutlet weak var interestSavings: UILabel!
+    @IBOutlet weak var yearsSavings: UILabel!
+    @IBOutlet weak var monthsSavings: UILabel!
+    
     @IBOutlet weak var extraPaymentSlider: UISlider!
     @IBOutlet weak var extraPaymentLabel: UILabel!
     @IBOutlet weak var paymentTypeControl: UISegmentedControl!
@@ -52,32 +61,75 @@ class EditPaymentVC: UIViewController {
 //        let remainingMonthsSaved = self.m!.monthsSaved() % 12
         //principal.text = "$" + String(describing: m!.loanAmount())
         //interest.text = "$" + String(describing: m!.totalLoanCost).components(separatedBy: ".")[0]
-        interestSavings.text! = "$" + String(describing: self.m?.originalMortgage!.totalLoanCost.subtracting(m!.totalLoanCost)).components(separatedBy: ".")[0]
+    }
+    
+    func updatePrincipalBalanceLabel(value: NSDecimalNumber) {
+        self.principalBalance.text! = "$" + String(describing: value).components(separatedBy: ".").first!
+    }
+    
+    func updateInterestBalanceLabel(value: NSDecimalNumber) {
+        self.interestBalance.text! = "$" + String(describing: value).components(separatedBy: ".").first!
+    }
+    
+    func updateTimeBalanceLabels(monthsLeft: Int) {
+        let yearsLeft = monthsLeft / 12
+        let monthsRemainder = monthsLeft % 12
+        
+        self.yearsBalance.text! = String(yearsLeft) + "yr"
+        self.monthsBalance.text! = String(monthsRemainder) + "mo"
+    }
+    
+    func updateExtraPaymentLabel(value: Float) {
+        extraPaymentLabel.text = "$" + String(Int(value))
+    }
+    
+    func updateInterestSavingsLabel(value: NSDecimalNumber) {
+        interestSavings.text = "$" + String(describing: value).components(separatedBy: ".")[0]
+    }
+    
+    func updateTimeSavingsLabels(months: Int) {
+        let yearsSaved = months / 12
+        let monthsSavedRemainder = months % 12
+        
+        self.yearsSavings.text! = String(yearsSaved) + "yr"
+        self.monthsSavings.text! = String(monthsSavedRemainder) + "mo"
     }
     
     @IBAction func sliderChanged(_ sender: UISlider) {
-        if paymentTypeControl.selectedSegmentIndex == 0 {
-            m?.extras = [["startMonth":1, "endMonth":360, "extraIntervalMonths":1, "extraAmount":Int(extraPaymentSlider.value)]]
-        } else {
-            m?.extras = [["startMonth":1, "endMonth":1, "extraIntervalMonths":1, "extraAmount":Int(extraPaymentSlider.value)]]
-        }
-        m = mc.calculateMortgage(mortgage: m!)
-        updateLabels()
+        updateMortgageMetrics()
     }
     
-    @IBAction func sliderChangedC(_ sender: UISlider) {
-        extraPaymentLabel.text = "$" + String(Int(extraPaymentSlider.value))
+    @IBAction func paymentTypeChanged(_ sender: UISegmentedControl) {
+        updateMortgageMetrics()
     }
     
-    
-    @IBAction func paymentTypeChanged(_ sender: Any) {
+    func updateMortgageMetrics() {
+        var extras = [Dictionary<String, Int>]()
+        
         if paymentTypeControl.selectedSegmentIndex == 0 {
-            m?.extras = [["startMonth":1, "endMonth":360, "extraIntervalMonths":1, "extraAmount":Int(extraPaymentSlider.value)]]
+            extras = [["startMonth":1, "endMonth":360, "extraIntervalMonths":1, "extraAmount":Int(extraPaymentSlider.value)]]  // TODO: Use current period as startMonth
         } else {
-            m?.extras = [["startMonth":1, "endMonth":1, "extraIntervalMonths":1, "extraAmount":Int(extraPaymentSlider.value)]]
+            extras = [["startMonth":1, "endMonth":1, "extraIntervalMonths":1, "extraAmount":Int(extraPaymentSlider.value)]]  // TODO: Use current period as start months
         }
-        m = mc.calculateMortgage(mortgage: m!)
-        updateLabels()
+        
+        let newMortgage = calculateNewMortgageFromExtras(mortgage: self.m!, newExtras: extras)
+        
+        updateExtraPaymentLabel(value: self.extraPaymentSlider.value)
+        updatePrincipalBalanceLabel(value: newMortgage.paymentSchedule.first!.remainingLoanBalance)  // TODO: Use current period
+        updateInterestBalanceLabel(value: newMortgage.totalLoanCost)
+        updateTimeBalanceLabels(monthsLeft: newMortgage.paymentSchedule.count)  // TODO: Subtract today's date from the paymentSchedule
+        updateInterestSavingsLabel(value: newMortgage.totalInterestSavings())
+        updateTimeSavingsLabels(months: newMortgage.originalMortgage!.paymentSchedule.count - newMortgage.paymentSchedule.count)
+        updateInterestSavingsLabel(value: newMortgage.totalInterestSavings())
+    }
+    
+    func calculateNewMortgageFromExtras(mortgage: Mortgage, newExtras: [Dictionary<String, Int>]) -> Mortgage {
+        var newMortgage = Mortgage(mortgage)
+        for extra in newExtras {
+            newMortgage.extras.append(extra)
+        }
+        newMortgage = mc.calculateMortgage(mortgage: newMortgage)
+        return newMortgage
     }
     
     func saveButton(sender: UIBarButtonItem) {
